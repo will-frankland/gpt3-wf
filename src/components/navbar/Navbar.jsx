f9cb951beda5c6072bec50fd73f43114c4b81d7b
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-13 23:25:37 -0500

    add menu styling for mobile devices and onClick handles for menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -24,7 +24,25 @@ const Navbar = () => {
         <button type='button'>Sign up</button>
       </div>
       <div className='gpt3__navbar-menu'>
-
+        {toggleMenu
+          ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(false)}/>
+        }
+        {toggleMenu && (
+          <div className='gpt3__navbar-menu_container scale-up-center'>
+            <div className='gpt3__navbar-menu_container-links'>
+                <p><a href='home'>Home</a></p>
+                <p><a href='#wgpt3'>What is GPT3?</a></p>
+                <p><a href='#possibility'>Open AI</a></p>
+                <p><a href='#features'>Case Studies</a></p>
+                <p><a href='#blog'>Library</a></p>
+            </div>
+            <div className='gpt3__navbar-sign'>
+              <p>Sign in</p>
+              <button type='button'>Sign up</button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
