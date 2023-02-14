ae1d9953d0337b8e4395f04ce26a269cdca8d53f
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-13 22:25:23 -0500

    move div closing tag so links are outside of logo container

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,6 +9,7 @@ const Navbar = () => {
       <div className='gpt3__navbar-links'>
         <div className='gpt3__navbar-links_logo'>
           <img src={logo} alt="logo" />
+        </div>
           <div className='gpt3__navbar-links_container'>
             <p><a href='home'>Home</a></p>
             <p><a href='#wgpt3'>What is GPT3?</a></p>
@@ -16,7 +17,6 @@ const Navbar = () => {
             <p><a href='#features'>Case Studies</a></p>
             <p><a href='#blog'>Library</a></p>
           </div>
-        </div>
       </div>
     </div>
   )
