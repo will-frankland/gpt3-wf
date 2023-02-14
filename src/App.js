ac5ced8b89c05d634aedf41a18c8df3c5b9a49ef
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-13 21:23:31 -0500

    add App.css with basic styling and import to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Footer, Blog, Possibilities, Features, WhatGPT3, Header } from './containers/index.js';
 import { CTA, Brand, Navbar } from './components';
+import './App.css';
 
 const App = () => {
   return (
