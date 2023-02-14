afc9e26f1adc0399697aadb3f78a27ee93c4356b
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-13 20:29:05 -0500

    add index.js file to export all components from containers as one liner

---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+import { default as Blog } from './blog/Blog';
+import { default as Features } from './features/Features';
+import { default as Footer } from './footer/Footer';
+import { default as Header } from './header/Header';
+import { default as Possibilities } from './possibilities/Possibilities';
+import { default as WhatGPT3 } from './whatGPT3/WhatGPT3';
