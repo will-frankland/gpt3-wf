1b910416e6af36bcdf505802febfcfee667094e9
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-13 21:44:12 -0500

    add imports and add div to Navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+
+      </div>
     </div>
   )
 }
