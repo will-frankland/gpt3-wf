d222eecc14a3fd7c280deaf0a3172f47bf17bf9d
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-13 20:39:03 -0500

    modify import to export in containers index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
-import { Footer, Blog, Possibilities, Features, WhatGPT3, Header } from './containers';
+import { Footer, Blog, Possibilities, Features, WhatGPT3, Header } from './containers/index.js';
 import { CTA, Brand, Navbar } from './components';
 
 const App = () => {
   return (
-    <div>
-      <h1>GPT3-WF</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibilities />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,6 +1,6 @@
-import { default as Blog } from './blog/Blog';
-import { default as Features } from './features/Features';
-import { default as Footer } from './footer/Footer';
-import { default as Header } from './header/Header';
-import { default as Possibilities } from './possibilities/Possibilities';
-import { default as WhatGPT3 } from './whatGPT3/WhatGPT3';
+export { default as Features } from './features/Features';
+export { default as Footer } from './footer/Footer';
+export { default as Header } from './header/Header';
+export { default as Possibilities } from './possibilities/Possibilities';
+export { default as WhatGPT3 } from './whatGPT3/WhatGPT3';
+export { default as Blog } from './blog/Blog';
