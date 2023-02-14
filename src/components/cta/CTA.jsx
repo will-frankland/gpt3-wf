f7f71090172b53084a48dfb6055114b6f1acbcae
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-14 16:07:58 -0500

    add styling to CTN and setup mobile compatibility

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -8,7 +8,7 @@ const CTA = () => {
         <p>Request Early Access to Get Started</p>
         <h3>Register today & start exploring the endless possiblities.</h3>
       </div>
-      <div className='gpt3__cta-button'>
+      <div className='gpt3__cta-btn'>
         <button type='button'>Get Started</button>
       </div>
     </div>
