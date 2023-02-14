ee745c9fc7b07e8de2d6fca502f0c0d042200fdc
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-13 20:27:05 -0500

    create containers folder with subfolders for containers, import css to each

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Article, Brand, CTA } from './components';
 
 const App = () => {
   return (
---
+++ src/containers/possibilities/Possibilities.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './possibilites.css';
+
+const Possibilities = () => {
+  return (
+    <div>
+      Possibilities
+    </div>
+  )
+}
+
+export default Possibilities;
