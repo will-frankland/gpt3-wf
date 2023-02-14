7051c88fb78832250bc4ad669f2978fc6ebcf044
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-13 21:32:33 -0500

    add gradient taken from angry tools to App.css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Footer, Blog, Possibilities, Features, WhatGPT3, Header } from './containers/index.js';
 import { CTA, Brand, Navbar } from './components';
 import './App.css';
+import './index.css';
 
 const App = () => {
   return (
