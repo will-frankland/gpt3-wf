87aed62ea51ba8c523228d921befac9e38546113
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-15 01:41:47 -0500

    add heading to blog section

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -3,8 +3,10 @@ import './blog.css';
 
 const Blog = () => {
   return (
-    <div>
-      
+    <div className='gpt3__blog section__padding' id="blog">
+      <div className='gpt3__blog-heading'>
+        <h1 className="gradient__text">A lot is happening. We are blogging about it.</h1>
+      </div>
     </div>
   )
 }
